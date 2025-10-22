05bb0b9389f29184bddc14f1e20e57967b70467f
Guilherme <guilhermegeorgetti4@gmail.com>
2025-10-22 13:49:41 -0300

    Atualização

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -2,6 +2,9 @@ import streamlit as st
 import plotly.express as px
 from Home import df_filtrado
 
+st.set_page_config(page_title = "App", layout = "wide")
+st.title("Gráficos de Indicadores")
+
 df_atrasados = df_filtrado.loc[df_filtrado["Status"] == "ATRASADO"]
 df_dentro = df_filtrado.loc[df_filtrado["Status"] != "ATRASADO"]
 
@@ -24,10 +27,24 @@ fig2 = px.pie(
 )
 fig2.update_layout(legend_title_text = "Cobrado")
 
+df_compradores = df_filtrado.groupby("Comprador")["Pedido"].count().sort_values(ascending=False).reset_index()
+df_compradores.columns = ["Comprador", "Quantidade Pedidos"]
+fig3 = px.bar(
+    df_compradores,
+    x = "Comprador",
+    y = "Nº de Pedidos",
+    title = "Quantidade de Pedidos por Comprador",
+    color_continuous_scale = "Blues"
+)
+
+
+#df_fornecedores = df_filtrado.groupby("Fornecedor")["Pedido"].count().sort_values(ascending=False).reset_index()
+
 col1, col2 = st.columns(2)
 
 with col1:
     st.plotly_chart(fig1)
+    st.plotly_chart(fig3)
 
 with col2:
     st.plotly_chart(fig2)
