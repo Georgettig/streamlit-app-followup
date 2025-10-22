f9f3d503d7efab1d375bfe94463a8dcec4cb4532
Guilherme <guilhermegeorgetti4@gmail.com>
2025-10-22 13:35:52 -0300

    Atualizacção

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -9,8 +9,7 @@ fig1 = px.pie(
     names = ["Atrasados", "Dentro do Prazo"],
     values = [len(df_atrasados), len(df_dentro)],
     hole = 0.4,
-    text_info = 
-    title = "Status dos Pedidos"
+    titte = "Status dos Pedidos"
 )
 fig1.update_layout(legend_title_text = "Status")
 
