661ed32410e8ca30be9399d16f4d3b37a42e4710
Guilherme <guilhermegeorgetti4@gmail.com>
2025-10-22 13:33:09 -0300

    Atualizacção

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
 from Home import df_filtrado
 
 df_atrasados = df_filtrado.loc[df_filtrado["Status"] == "ATRASADO"]
@@ -10,9 +9,10 @@ fig1 = px.pie(
     names = ["Atrasados", "Dentro do Prazo"],
     values = [len(df_atrasados), len(df_dentro)],
     hole = 0.4,
-    color_discrete_map = {"Atrasados":"red", "Dentro do Prazo":"green"},
+    text_info = 
     title = "Status dos Pedidos"
 )
+fig1.update_layout(legend_title_text = "Status")
 
 df_cobrados = df_filtrado.loc[df_filtrado["Cobrado"] == "COBRADO"]
 df_nao_cobrados = df_filtrado.loc[df_filtrado["Cobrado"] != "COBRADO"]
@@ -21,9 +21,9 @@ fig2 = px.pie(
     names = ["Cobrados", "Não Cobrados"],
     values = [len(df_cobrados), len(df_nao_cobrados)],
     hole = 0.4,
-    color_discrete_map = {"Cobrados":"green", "Não Cobrados":"red"},
     title = "Situação dos Pedidos"
 )
+fig2.update_layout(legend_title_text = "Cobrado"))
 
 col1, col2 = st.columns(2)
 
